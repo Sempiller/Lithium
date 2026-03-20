d1621937a022f1d41ec5b42c4c28cca3173f5590
sempiller <110603660+Sempiller@users.noreply.github.com>
2026-03-21 00:19:25 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,8 +11,15 @@ game:GetService("StarterGui"):SetCore("SendNotification", {
         Text = "Name credit goes to lithium.1on on discord.",
         Duration = 5
     })
-                                                   
-                                                   
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Lithium",
+        Text = "Please use a good executor.",
+        Duration = 5
+    })
+
+wait(3)
+
 local placeId = game.PlaceId
 
 if placeId == 286090429 then
