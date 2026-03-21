8b326c993610ba6211a711910c1c13d4330942aa
sempiller <110603660+Sempiller@users.noreply.github.com>
2026-03-21 22:21:16 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,16 +9,25 @@ print("███████ ██    ██    ██   ██ ██  ██�
 game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "Lithium",
         Text = "Name credit goes to lithium.1on on discord.",
-        Duration = 5
+        Duration = 3
     })
 
 game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "Lithium",
         Text = "Please use a good executor.",
-        Duration = 5
+        Duration = 3
     })
 
 wait(3)
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Pluto",
+	Text = "Use our partner executor, Pluto. | https://pluto.ad/ ", 
+	Icon = "rbxassetid://116995774410209" 
+        Duration = 20
+})
+
+wait(5)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Arsenal.lua"))()
 
