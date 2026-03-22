af0435445e4cfd143be78b805f89dfebeb1fd82c
sempiller <110603660+Sempiller@users.noreply.github.com>
2026-03-22 22:45:57 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,33 +1,9 @@
-print("____________________________________________________________________________________________________")
-print("")
-print("██      ██ ████████ ██   ██ ██ ██    ██ ███    ███ ")
-print("██      ██    ██    ██   ██ ██ ██    ██ ████  ████ ")
-print("██      ██    ██    ███████ ██ ██    ██ ██ ████ ██ ")
-print("██      ██    ██    ██   ██ ██ ██    ██ ██  ██  ██ ")
-print("███████ ██    ██    ██   ██ ██  ██████  ██      ██ ")
-
-game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Lithium",
-        Text = "Name credit goes to lithium.1on on discord.",
+if game.PlaceId == 286090429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Arsenal.lua"))()
+else
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Lithium XV",
+        Text = "Game not supported yet.",
         Duration = 3
     })
-
-game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Lithium",
-        Text = "Please use a good executor.",
-        Duration = 3
-    })
-
-wait(3)
-
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "Pluto",
-	Text = "Use our partner executor, Pluto. | https://pluto.ad/ ", 
-	Icon = "rbxassetid://116995774410209" 
-        Duration = 20
-})
-
-wait(5)
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Arsenal.lua"))()
-
+end
