54d7ec5ed98b9ef96e7b224ee5d9112ea8689aa4
sempiller <110603660+Sempiller@users.noreply.github.com>
2026-03-31 22:34:21 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,1 @@
-if game.PlaceId == 286090429 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Arsenal.lua"))()
-else
-    game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Lithium XV",
-        Text = "Game not supported yet.",
-        Duration = 3
-    })
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Arsenal.lua"))()
