c6d687998647e9f95759c2bde1083deb84da05e4
sempiller <110603660+Sempiller@users.noreply.github.com>
2026-05-01 16:28:01 +0300

    Prompt players to join Discord
    Add Discord link prompt for players.

--- main.lua
+++ main.lua
@@ -1,1 +1,11 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Arsenal.lua"))()
+
+local DISCORD_LINK = "https://discord.gg/Cxt5YjFTaP" 
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+if player then
+    player:PromptOpenURL(DISCORD_LINK)
+    print("pls join discord")
+else
+    warn("unable to find localplayer")
+end
