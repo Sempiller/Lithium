32b405457ddcafec0e20a2718364ab9a5a4dd1b6
sempiller <110603660+Sempiller@users.noreply.github.com>
2026-05-25 23:34:29 +0300

    Remove comment and duplicate loadstring call

--- Criminality.lua
+++ Criminality.lua
@@ -1,3 +1,2 @@
-
 getgenv().SCRIPT_KEY = "5e170ec2-64b0-4ff8-b6ba-740042a19845" -- Don't change the KEY so script can load
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/00602ff36af5f4f708f3dcc4aca05f0264830cdbcfb36f69ea317d3296f86d89/download"))()
