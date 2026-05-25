8564b9e3511011ce13d208ac4440e989c777e1e2
sempiller <110603660+Sempiller@users.noreply.github.com>
2026-05-25 22:11:08 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
-if game.PlaceId == 4588604953 then 
-    -- Criminality
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local placeId = game.PlaceId
+
+if placeId == 4588604953 or placeId == 8343259840 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Criminality.lua"))()
-    
-elseif game.PlaceId == 286090429 then 
-    -- Arsenal
+
+elseif placeId == 286090429 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Arsenal.lua"))()
+else
+    print("Lithium Loader: Unregistered PlaceId -> " .. tostring(placeId))
 end
