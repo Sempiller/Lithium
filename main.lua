83804bbefbbd33ae0cdc144a445a2bac698fa5ce
sempiller <110603660+Sempiller@users.noreply.github.com>
2026-05-25 21:27:45 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Arsenal.lua"))()
+if game.PlaceId == 4588604953 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Criminality.lua"))()
+elseif game.PlaceId == 286090429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Arsenal.lua"))()
+end
