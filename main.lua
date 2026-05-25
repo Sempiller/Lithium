9fa699c76e00b47eb8b64523d60d0407d3e27ab3
sempiller <110603660+Sempiller@users.noreply.github.com>
2026-05-25 22:04:36 +0300

    Refactor game.PlaceId checks for loading scripts

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
-if game.PlaceId == 4588604953 then
+if game.PlaceId == 4588604953 then 
+    -- Criminality
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Criminality.lua"))()
-elseif game.PlaceId == 286090429 then
+    
+elseif game.PlaceId == 286090429 then 
+    -- Arsenal
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/Lithium/refs/heads/main/Arsenal.lua"))()
 end
